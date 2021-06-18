4067637e0f7ee9ace8a3045ad57d756e1ed1a818
Joshua Verdehem <joshverd2@gmail.com>
2021-06-18 15:20:46 -0500

    Init

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as reactChartJS from 'react-chartjs-2';
+
+ReactDOM.render(
+  <reactChartJS.Bar
+    options={{}}
+    type="bar"    
+    data={[]} 
+  />,
+  document.getElementById('root')
+);
